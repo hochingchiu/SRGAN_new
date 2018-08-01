a6c266f73acb1860049c43a9159303289958d7ac
Ho Ching Chiu <39039013+hochingchiu@users.noreply.github.com>
2018-08-01 20:43:48 +0900

    swapped play and backup.py

--- test_image-play.py
+++ test_image-play.py
@@ -8,17 +8,18 @@ from torchvision.transforms import ToTensor, ToPILImage
 
 from model import Generator
 
-parser = argparse.ArgumentParser(description='Test Single Image')
-parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
-parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
-parser.add_argument('--image_name', type=str, help='test low resolution image name')
-parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
-opt = parser.parse_args()
+#parser = argparse.ArgumentParser(description='Test Single Image')
+#parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
+#parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
+#parser.add_argument('--image_name', type=str, help='test low resolution image name')
+#parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
+#opt = parser.parse_args()
 
-UPSCALE_FACTOR = opt.upscale_factor
-TEST_MODE = True if opt.test_mode == 'GPU' else False
-IMAGE_NAME = opt.image_name
-MODEL_NAME = opt.model_name
+path = '/data/play/'
+UPSCALE_FACTOR = 4
+TEST_MODE = True #if opt.test_mode == 'GPU' else False
+IMAGE_NAME = path + '001.jpg'
+MODEL_NAME = 'netG_epoch_4_100.pth'
 
 model = Generator(UPSCALE_FACTOR).eval()
 if TEST_MODE:
@@ -37,4 +38,4 @@ out = model(image)
 elapsed = (time.clock() - start)
 print('cost' + str(elapsed) + 's')
 out_img = ToPILImage()(out[0].data.cpu())
-out_img.save('out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
+out_img.save(path + 'out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
