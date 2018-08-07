1ca1acd602b3da24a6cf21b2e8a2e468f460e7d0
Ho Ching Chiu <39039013+hochingchiu@users.noreply.github.com>
2018-08-08 01:19:42 +0900

    reduced feature map size

--- .ipynb_checkpoints/model_inception-checkpoint.py
+++ .ipynb_checkpoints/model_inception-checkpoint.py
@@ -120,18 +120,18 @@ class InceptionBlock(nn.Module):
 
         self.prelu = nn.PReLU()
  
-        self.branch1x1 = BasicConv2d(in_channels, 64, kernel_size=1) 
+        self.branch1x1 = BasicConv2d(in_channels, 32, kernel_size=1) 
 
-        self.branch5x5_1 = BasicConv2d(in_channels, 48, kernel_size=1)
-        self.branch5x5_2 = BasicConv2d(48, 64, kernel_size=5, padding=2)
+        self.branch5x5_1 = BasicConv2d(in_channels, 24, kernel_size=1)
+        self.branch5x5_2 = BasicConv2d(24, 32, kernel_size=5, padding=2)
 
-        self.branch3x3dbl_1 = BasicConv2d(in_channels, 64, kernel_size=1)
-        self.branch3x3dbl_2 = BasicConv2d(64, 96, kernel_size=3, padding=1)
-        self.branch3x3dbl_3 = BasicConv2d(96, 96, kernel_size=3, padding=1)
+        self.branch3x3dbl_1 = BasicConv2d(in_channels, 32, kernel_size=1)
+        self.branch3x3dbl_2 = BasicConv2d(32, 48, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = BasicConv2d(48, 48, kernel_size=3, padding=1)
 
-        self.branch_pool = BasicConv2d(in_channels, 64, kernel_size=1)
+        self.branch_pool = BasicConv2d(in_channels, 32, kernel_size=1)
         
-        self.reduce = BasicConv2d(288, 64, kernel_size=1) 
+        self.reduce = BasicConv2d(144, 64, kernel_size=1) 
 
     def forward(self, x):
         branch1x1 = self.branch1x1(x)
